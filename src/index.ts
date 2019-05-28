02ccaf299359fad10b379b9c78cf7a14050ae31a
Lucas Malandrino <lucas.malandrino@gmail.com>
2019-05-28 17:31:41 +0200

    Reorganized project. Created subprojects. Easy to publish

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import { Polyglot } from './graal-compatibility';
-import { Channel, MessageChannel } from './@jda/entities';
-
-function test() {
-    const a: MessageChannel = Polyglot.import('aChannel');
-    a.sendMessage("test").queue();
-}
